74b78b0d3e77d8e006568b654129ae7217dcd1d0
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-04-20 17:43:19 +0200

    added router to the app middleware

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import router from './router/router'
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/api', router)
 
 
 app.listen(PORT, () => {
