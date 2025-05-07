f6c575be5b7f0a612e71e84c082892a68f92ad19
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-05-07 18:02:35 +0200

    added parse json

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(cors())
+app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/api', router)
